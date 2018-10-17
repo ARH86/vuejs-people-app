c1c0720268ff385409c0ffe0131a8604b2153809
Adam <ahanson845@gmail.com>
2018-10-16 21:37:19 -0500

    updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,84 @@
 <template>
   <div class="home">
-    <h1> People </h1>
-  </div>
-  <div>
-    <input v-model="newPerson.text">
-    <button v-on:click="addPerson()">Add Person</button>
-  </div>
-  <div>
-    
+    <h1>Interesting People</h1>
+
+    <div>
+      Name: <input v-model="newPerson.name">  
+      Bio: <input v-model="newPerson.bio">
+      <button v-on:click="addPerson()">Add Person</button>
+    </div>
+
+    <div>
+      <h4>Total Number of People: {{ people.length }}</h4>
+    </div>
+
+    <div v-for="person in people">
+      <h2 @click="toggleBio(person)">{{ person.name }}</h2>
+      <div v-bind:class="{strike: person.bioVisible}">  
+        <h3>{{ person.bio }}</h3>
+      <button @click="deletePerson(person)">Delete</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
+  .strike {
+      text-decoration: line-through;
+
+  }
+
 </style>
 
 <script>
+var axios = require('axios');
+
 export default {
   data: function() {
     return {
-      bio: [
-          {name: "Joe", bio:"Born in a sahara desert ", bioVisible: true },
-          {name: "Dill", bio:"Never had a parent ", bioVisible: true },
-          {name: "Garlin", bio:"Lives his life spicy", bioVisible: true }
-      ],
-      newPerson: {name: '', bio: '', bioVisible: truep}
+      people: [],
+
+      newPerson: {name: "", bio: "", bioVisible: true}
     };
   },
-  created: function() {},
-  methods: {},
+  created: function() {
+    axios.get("http://localhost:3000/api/people").then(function(response){ 
+      this.people = (response.data);
+    }.bind(this));
+  },
+  methods: {
+    toggleBio: function(inputPerson) {
+      inputPerson.bioVisible = !inputPerson.bioVisible;
+    },
+    addPerson: function() {
+      if (this.newPerson.name && this.newPerson.bio) {
+        this.people.push(this.newPerson);
+        this.newPerson = {name: "", bio: "", bioVisible: true};
+      }
+    },
+    deletePerson: function(inputPerson) {
+      var index = this.people.indexOf(inputPerson);
+      this.people.splice(index, 1);
+    }
+  },
   computed: {}
 };
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
